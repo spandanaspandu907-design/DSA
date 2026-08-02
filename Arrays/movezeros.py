c125a106566c6fe4c3bb0fc558eb268d208fceb2
Spandana B <spandanaspandu907@gmail.com>
2026-08-02 13:17:38 +0530

    DSA Day2

---
+++ Arrays/movezeros.py
@@ -0,0 +1,8 @@
+class Solution:
+    def movezeroes(self, nums):
+        j = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
