381f2f2622c0724d841eba1a27fcc59bcc5550e5
Spandana B <spandanaspandu907@gmail.com>
2026-08-10 17:19:48 +0530

    Day 8

---
+++ Arrays/topkFrequent.py
@@ -0,0 +1,9 @@
+class Solution:
+    def topkFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        return sorted(freq, key=freq.get, reverse=True)[:k]
+        
