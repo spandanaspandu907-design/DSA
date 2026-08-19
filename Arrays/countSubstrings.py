2e5bc6a854781ce09a007f8ee8a23e14d9e0bc75
Spandana B <spandanaspandu907@gmail.com>
2026-08-19 19:58:50 +0530

    Day13

---
+++ Arrays/countSubstrings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countSubstrings(self, s):
+        ans = 0
+
+        def expand(left, right):
+            count = 0
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+
+            return count
+
+        for i in range(len(s)):
+            ans += expand(i, i)       # odd length
+            ans += expand(i, i + 1)   # even length
+
+        return ans
