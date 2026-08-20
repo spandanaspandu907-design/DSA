aff08606dcac0f358a75e777a2279858f988f9d4
Spandana B <spandanaspandu907@gmail.com>
2026-08-20 09:49:38 +0530

    Day14

---
+++ Arrays/isIsomorphic.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        st = {}
+        ts = {}
+
+        for a, b in zip(s, t):
+            if (a in st and st[a] != b) or (b in ts and ts[b] != a):
+                return False
+
+            st[a] = b
+            ts[b] = a
+
+        return True
+
+        
